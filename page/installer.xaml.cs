19cca8cb72ea5d2dd99fa6f4fa53d59befb50f95
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2026-01-04 15:41:50 +0200

    Add files via upload

---
+++ page/installer.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System;
+using System.Windows.Controls;
+
+namespace Redemption_Team.page
+{
+    public partial class InstallerControl : UserControl
+    {
+        public InstallerControl()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.Description = "اختر مسار تثبيت التعريب";
+                dialog.ShowNewFolderButton = true;
+
+                //dialog.SelectedPath = InstallPathTextBox.Text;
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string selectedPath = dialog.SelectedPath;
+
+                    if (!selectedPath.EndsWith("Red Dead Redemption 2", StringComparison.OrdinalIgnoreCase))
+                    {
+                        System.Windows.MessageBox.Show(
+                            "يرجى اختيار المسار الصحيح",
+                            "خطأ في المسار",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Warning
+                        );
+                        return;
+                    }
+
+                    //InstallPathTextBox.Text = selectedPath;
+                }
+            }
+        }
+
+    }
+}
